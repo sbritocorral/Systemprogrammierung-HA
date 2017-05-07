74c780ccc4210b1b4863876c316af858d139ea1a
sbritocorral <s.britocorral@gmail.com>
2017-05-07 16:20:48 +0200

    Avances

--- PrioQueue.c
+++ PrioQueue.c
@@ -19,24 +19,65 @@ struct PrioQueue
 
 PrioQueue* pqueue_new()
 {
-	//TODO
-	return NULL;
+	PrioQueue *new = malloc(sizeof(PrioQueue));
+	return new;
 }
 
 q_elem* q_elem_new(int priority, char* name)
 {
-	//TODO
-	return NULL;
+	q_elem * new_elem = malloc(sizeof(q_elem_s));
+	
+	new_elem->priority = priority;	
+	strcpy(new_elem->name, name);
+	
+	return new_elem;
 }
 
 void pqueue_free(PrioQueue *queue)
 {
-	//TODO
+	q_elem * tmp = null;
+	
+	while(queue->root != null){
+		
+		tmp= queue->root;
+		queue->root = queue->root->next;
+		
+		free(tmp);	
+	}
+
 }
 
 char* pqueue_offer(PrioQueue *queue, q_elem* newElem)
 {
-	//TODO
+	
+	q_elem *tmp1 = queue->root;
+	q_elem *tmp2 = queue->root->next;
+	
+	
+	if(queue->root->next != null){
+		
+		if(newElem->priority)
+		
+	}
+	
+	while(tmp2!=null){
+		
+		if(newElem->priority > tmp2->priority){
+			
+			tmp1->next = newElem;
+			newElem->next = tmp2;	
+		}
+		else{		
+			newElem->next = tmp2->next;
+			tmp2->next = newElem;
+		}
+			
+		tmp1 = tmp1->next;
+		tmp2 = tmp2->next;
+	}
+	
+	
+	
 	return NULL;
 }
 
