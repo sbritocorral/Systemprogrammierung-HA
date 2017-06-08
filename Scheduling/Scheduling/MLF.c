19dcc763c8febd4d8de074adf50157ec274303d2
Sebastian Brito <s.britocorral@gmail.com>
2017-06-08 15:26:38 +0000

    RR and MLF

--- Scheduling/Scheduling/MLF.c
+++ Scheduling/Scheduling/MLF.c
@@ -1,6 +1,137 @@
 #include "MLF.h"
+#include "Queue.h"
+#include <math.h>
+#include <stdlib.h>
 
-void schedule_MLF(const TaskPool *task_pool, uint16_t num_levels) {
-    // TODO
-    printf("NOT IMPLEMENTED!\n");
+void schedule_MLF(const TaskPool *task_pool, uint16_t num_levels)
+{
+
+    int tick = 0;
+    int quantum = 0;
+
+    int numPiscina = 0;
+
+    Queue **piscina = malloc(sizeof(Queue *) * (num_levels + 1));
+
+    for (int i = 0; i < num_levels; i++)
+    {
+        piscina[i] = queue_new();
+    }
+
+    while (!allDone(task_pool))
+    {
+        Task *taskArrive = checkArrivals(task_pool, tick);
+
+        task_elem *tmp = NULL;
+
+        if (taskArrive != NULL)
+        {
+            offer(piscina[0], task_elem_new(taskArrive));
+        }
+
+        Task *gg = NULL;
+
+        for (int i = 0; i < num_levels; i++)
+        {
+
+            if ((gg = queue_poll(piscina[i])) != NULL)
+            {
+
+                tmp = task_elem_new(gg);
+
+                numPiscina = i;
+                break;
+            }
+        }
+
+        if (tmp != NULL)
+        {
+            if (numPiscina == num_levels - 1)
+            {
+                int remainingTicks = tmp->task->total_ticks - tmp->task->exec_ticks;
+                for (int i = 0; i < remainingTicks; i++)
+                {
+                    if (!isDone(tmp->task))
+                    {
+                        if (execTask(tmp->task, 1) < 0)
+                        {
+                            printf("%sERROR:%s No Task selected to be executed.\n", COLOR_RED, COLOR_RESET);
+                            break;
+                        }
+                    }
+
+                    if (i != remainingTicks - 1)
+                    {
+                        tick++;
+                        Task *taskArrive = checkArrivals(task_pool, tick);
+
+                        if (taskArrive != NULL)
+                        {
+                            offer(piscina[0], task_elem_new(taskArrive));
+                        }
+                    }
+                }
+
+                free(tmp);
+            }
+
+           // Aqui me repito para caso de queue y fcfs
+
+            else
+            {
+                quantum = (int)pow(2.0, (double)numPiscina);
+
+                //printf("Esto es quantum : %d\n", quantum);
+                for (int i = 0; i < quantum; i++)
+                {
+                    if (!isDone(tmp->task))
+                    {
+                        if (execTask(tmp->task, 1) < 0)
+                        {
+                            printf("%sERROR:%s No Task selected to be executed.\n", COLOR_RED, COLOR_RESET);
+                            break;
+                        }
+                    }
+
+                    if (i != quantum - 1)
+                    {
+                        tick++;
+                        Task *taskArrive = checkArrivals(task_pool, tick);
+
+                        if (taskArrive != NULL)
+                        {
+                            offer(piscina[0], task_elem_new(taskArrive));
+                        }
+                    }
+                }
+
+                if (tmp->task->exec_ticks != tmp->task->total_ticks)
+                {
+                    if (numPiscina != num_levels - 1)
+                    {
+                        offer(piscina[numPiscina + 1], tmp);
+                    }
+                }
+                else{
+                    free(tmp);
+                }
+
+                
+            }
+
+            
+        }
+
+        tick++;
+    }
+
+    for (int i = 0; i < num_levels; i++)
+    {
+        queue_free(piscina[i]);
+    }
+
+    free(piscina);
+
+    printf("\n");
 }
+
--- Scheduling/Scheduling/RR.c
+++ Scheduling/Scheduling/RR.c
@@ -1,6 +1,64 @@
 #include "RR.h"
+#include "Queue.h"
+#include <stdlib.h>
 
 void schedule_RR(const TaskPool *task_pool, uint16_t quantum_max) {
-    // TODO
-    printf("NOT IMPLEMENTED!\n");
+    
+    int tick = 0;
+    int quantum = quantum_max;
+
+    Queue * queue = queue_new();
+
+    while (!allDone(task_pool))
+    {
+        Task *taskArrive = checkArrivals(task_pool, tick);
+
+        task_elem* tmp = NULL;
+
+        if (taskArrive != NULL)
+        {
+            offer(queue, task_elem_new(taskArrive));
+        }
+
+        tmp = task_elem_new(queue_poll(queue));
+        
+        if(tmp != NULL){
+            
+            for(int i = 0; i<quantum; i++){
+              
+                if(!isDone(tmp->task))
+                {
+                    if (execTask(tmp->task, 1) < 0)
+                    {
+                        printf("%sERROR:%s No Task selected to be executed.\n", COLOR_RED, COLOR_RESET);
+                        break;
+                    }
+                }
+                
+                if(i != quantum - 1)
+                {
+                    tick++;
+                    Task *taskArrive = checkArrivals(task_pool, tick);
+
+                    if(taskArrive != NULL){
+                        offer(queue, task_elem_new(taskArrive));
+                    }
+                }
+                             
+            }
+
+            if(tmp->task->exec_ticks != tmp->task->total_ticks){    
+                offer(queue, tmp);
+            }
+            else{
+                free(tmp);
+            }
+       }
+
+        tick++;
+    }
+
+    queue_free(queue);
+    
+    printf("\n");
 }
