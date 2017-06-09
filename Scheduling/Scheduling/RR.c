2465871a030ec6835aa15e8b18a0593d30276e16
Sebastian Brito <s.britocorral@gmail.com>
2017-06-09 12:51:09 +0000

    Round Robin bug fixed

--- Scheduling/Scheduling/RR.c
+++ Scheduling/Scheduling/RR.c
@@ -2,31 +2,46 @@
 #include "../Queue/queue.h"
 #include <stdlib.h>
 
-void schedule_RR(const TaskPool *task_pool, uint16_t quantum_max) {
-    
+void schedule_RR(const TaskPool *task_pool, uint16_t quantum_max)
+{
+
     int tick = 0;
     int quantum = quantum_max;
 
-    Queue * queue = queue_new();
+    Queue *queue = queue_new();
+
+    task_elem *tmp = NULL;
 
     while (!allDone(task_pool))
     {
         Task *taskArrive = checkArrivals(task_pool, tick);
-
-        task_elem* tmp = NULL;
 
         if (taskArrive != NULL)
         {
             offer(queue, task_elem_new(taskArrive));
         }
 
+         if (tmp != NULL)
+        {
+            if (tmp->task->exec_ticks != tmp->task->total_ticks)
+            {
+                offer(queue, tmp);
+            }
+            else
+            {
+                free(tmp);
+            }
+        }
+        tmp = NULL;
         tmp = task_elem_new(queue_poll(queue));
-        
-        if(tmp != NULL){
-            
-            for(int i = 0; i<quantum; i++){
-              
-                if(!isDone(tmp->task))
+
+        if (tmp != NULL)
+        {
+
+            for (int i = 0; i < quantum; i++)
+            {
+
+                if (!isDone(tmp->task))
                 {
                     if (execTask(tmp->task, 1) < 0)
                     {
@@ -34,31 +49,24 @@ void schedule_RR(const TaskPool *task_pool, uint16_t quantum_max) {
                         break;
                     }
                 }
-                
-                if(i != quantum - 1)
+
+                if (i != quantum - 1)
                 {
                     tick++;
                     Task *taskArrive = checkArrivals(task_pool, tick);
 
-                    if(taskArrive != NULL){
+                    if (taskArrive != NULL)
+                    {
                         offer(queue, task_elem_new(taskArrive));
                     }
                 }
-                             
             }
-
-            if(tmp->task->exec_ticks != tmp->task->total_ticks){    
-                offer(queue, tmp);
-            }
-            else{
-                free(tmp);
-            }
-       }
+        }
 
         tick++;
     }
 
     queue_free(queue);
-    
+
     printf("\n");
 }
